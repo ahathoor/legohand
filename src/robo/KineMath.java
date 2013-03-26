a898bd3e5f90c2033893d5a560878fb493e57420
Mikko Tamminen <pahanaama@gmail.com>
2013-03-26 02:17:22 +0200

    KineMath can now choose a fitting arm solution to suit given constraints

--- src/robo/KineMath.java
+++ src/robo/KineMath.java
@@ -6,26 +6,25 @@ public class KineMath {
 		KineMathTest kt = new KineMathTest();
 		kt.run();
 	}
+
 	public static double bestAngleInDeg(double[] xy, double dist, double target) {
-		return Math.toDegrees(bestAngle(xy, dist, target));
+		return Math.toDegrees(bestAngleCW(xy, dist, target));
 	}
-	public static double bestAngle(double[] xy, double dfromxy, double target) {
-		/**
-		 * Searches the best angle to go a distance <b>dist</b> from (x,y) so that distance from 
-		 * origo is as close to target as possible
-		 */
-		xy = new double[] {xy[0] , -xy[1] };
-		double min = Math.PI - Math.atan2(xy[1], xy[0]);
-		double max = min - 2 * Math.PI;
+
+	private static double binSearchAngle(double[] xy, double dfromxy,
+			double target, double min, double max) {
+
 		double search = 0;
 		for (int i = 0; i < 70; i++) {
 			if (Math.abs(min - max) == 0)
 				break;
 			search = (min + max) / 2;
-			double k1 = Math
-					.abs(distToOrigo(kärki(xy[0], xy[1], dfromxy, (search + min) / 2)) - target);
-			double k2 = Math
-					.abs(distToOrigo(kärki(xy[0], xy[1], dfromxy, (search + max) / 2)) - target);
+			double k1 = Math.abs(distToOrigo(kärki(xy[0], xy[1], dfromxy,
+					(search + min) / 2))
+					- target);
+			double k2 = Math.abs(distToOrigo(kärki(xy[0], xy[1], dfromxy,
+					(search + max) / 2))
+					- target);
 			if (k1 < k2) {
 				max = search;
 			} else {
@@ -35,35 +34,67 @@ public class KineMath {
 
 		return positivifyAngle(search);
 	}
-	public static double[] binSearchPointWithDist(double[] xy, double dfromxy, double target) {
-		double search = bestAngle(xy, dfromxy, target);
-		return new double[] {xy[0] + Math.cos(search)*dfromxy, xy[1] + Math.sin(search)*dfromxy};
+
+	public static double bestAngleCW(double[] xy, double dfromxy, double target) {
+		double min = angleBetween(xy, new double[] { 0, 0 }) - Math.PI / 2;
+		double max = min + Math.PI / 2;
+		return binSearchAngle(xy, dfromxy, target, min, max);
 	}
-	
+
+	public static double bestAngleCCW(double[] xy, double dfromxy, double target) {
+		double min = angleBetween(xy, new double[] { 0, 0 }) + Math.PI / 2;
+		double max = min - Math.PI / 2;
+		return binSearchAngle(xy, dfromxy, target, min, max);
+	}
+
+	public static double[] bestPointCW(double[] xy, double dfromxy,
+			double target) {
+		double search = bestAngleCW(xy, dfromxy, target);
+		return kärki(xy, dfromxy, search);
+	}
+
+	public static double[] bestPointCCW(double[] xy, double dfromxy,
+			double target) {
+		double search = bestAngleCCW(xy, dfromxy, target);
+		return kärki(xy, dfromxy, search);
+	}
+
 	public static double angleBetween(double[] xy1, double[] xy2) {
 		return Math.atan2(xy2[1] - xy1[1], xy2[0] - xy1[0]);
 	}
 
 	public static double[] etsikulmat(double[] xy, double armFromSwiwel, double armFromOrigo, double[] nearRange, double[] farRange) {
-		double[] a = etsikulmat(xy, armFromSwiwel, armFromOrigo);
-		a = new double[] {constrainAngle(a[0], nearRange),
-				constrainAngle(a[1], farRange)};
-		if(Double.isNaN(a[0]) || Double.isNaN(a[1]))
-			return null;
-		return a;
-	}
-	public static double[] etsikulmat(double[] xy, double armFromSwiwel, double armFromOrigo) {
+		double[] betweenPoint = bestPointCW(xy, armFromSwiwel, armFromOrigo);
+		double k1 = Math.toDegrees(angleBetween(new double[] {0,0}, betweenPoint));
+		double k2 = Math.toDegrees(angleBetween(betweenPoint, xy));
+		k2-=k1;
 		
-		double[] betweenPoint = binSearchPointWithDist(xy, armFromSwiwel, armFromOrigo);
-		double k1 = angleBetween(new double[] {0,0}, betweenPoint);
+		if(!fitsInRange(k1, nearRange) || !fitsInRange(k2, farRange)){
+			betweenPoint = bestPointCCW(xy, armFromSwiwel, armFromOrigo);
+			k1 = Math.toDegrees(angleBetween(new double[] {0,0}, betweenPoint));
+			k2 = Math.toDegrees(angleBetween(betweenPoint, xy));
+			k2-=k1;
+		if(!fitsInRange(k1, nearRange) || !fitsInRange(k2, farRange))
+			return null;
+		}
+		return new double[] {k1,k2};
+	}
+
+	public static double[] etsikulmat(double[] xy, double armFromSwiwel,
+			double armFromOrigo) {
+
+		double[] betweenPoint = bestPointCW(xy, armFromSwiwel, armFromOrigo);
+		double k1 = angleBetween(new double[] { 0, 0 }, betweenPoint);
 		double k2 = angleBetween(betweenPoint, xy);
 		k2 -= k1;
-		return new double[] { Math.toDegrees(positivifyAngle(k1)), Math.toDegrees(positivifyAngle(k2)) };
+		return new double[] { Math.toDegrees(k1), Math.toDegrees(k2) };
 	}
 
 	public static double positivifyAngle(double a) {
-		while (a+0.1 < 0)
+		while (a + 0.1 < 0)
 			a += Math.PI * 2;
+		if (a > Math.PI * 2)
+			a -= Math.PI * 2;
 		return a;
 	}
 
@@ -71,23 +102,21 @@ public class KineMath {
 		return Math.sqrt(piste[0] * piste[0] + piste[1] * piste[1]);
 	}
 
-	public static double[] kärki(double x, double y, double r, double angle) {
-		double rx = x + Math.cos(angle) * r;
-		double ry = y - Math.sin(angle) * r;
-		return new double[] { rx, ry };
+	public static double[] kärki(double[] xy, double r, double angle) {
+		return kärki(xy[0], xy[1], r, angle);
 	}
-	
-	public static double constrainAngle(double angle, double[] constraint){
-		/**
-		 * constraints an angle to the given range, and return NaN if not possible;
-		 */
+
+	public static double[] kärki(double x, double y, double r, double angle) {
+		return new double[] { x + Math.cos(angle) * r, y + Math.sin(angle) * r };
+	}
+
+	public static boolean fitsInRange(double angle, double[] constraint) {
 		double min = constraint[0];
 		double max = constraint[1];
-		angle = positivifyAngle(angle);
 		if (angle > max)
 			angle -= 360;
 		if (angle < min)
-			return Double.NaN;
-		return angle;
+			return false;
+		return true;
 	}
 }
--- src/robo/KineMathTest.java
+++ src/robo/KineMathTest.java
@@ -2,8 +2,8 @@ package robo;
 
 public class KineMathTest {
 
-//	KineMath k = new KineMath();
-	
+	// KineMath k = new KineMath();
+
 	public void run() {
 		testAngleBetweenxx00();
 		testAngleBetween00xx();
@@ -14,169 +14,213 @@ public class KineMathTest {
 		testBinSearchStraightRight();
 		testEtsiKulmat();
 		testBestAngle();
+
+		karkiTest();
 	}
+
+	public void karkiTest() {
+		double[] p = KineMath.kärki(new double[] { 0, 0 }, 50, Math.PI / 4);
+		assertEqual("kärkitest", Math.sqrt(50 * 50 / 2), p[0], 0.1);
+
+		p = KineMath.kärki(new double[] { 0, 0 }, 50, 0);
+		assertEqual("kärkitest2", 50, p[0], 0.1);
+
+	}
+
 	public void testBestAngle() {
-		double[] p = {0,100};
+		double[] p = { 0, 100 };
 		double angle = KineMath.bestAngleInDeg(p, 10, 90);
 		assertEqual("Best angle straight up", 270, angle, 0.1);
-		
-		p = new double[] {0,-100};
+
+		p = new double[] { 0, -100 };
 		angle = KineMath.bestAngleInDeg(p, 10, 90);
 		assertEqual("Best angle straight down", 90, angle, 0.1);
-		
 
-		p = new double[] {-100,0};
+		p = new double[] { -100, 0 };
 		angle = KineMath.bestAngleInDeg(p, 10, 90);
 		assertEqual("Best angle straight left", 0, angle, 0.1);
-		
 
-		p = new double[] {100,0};
+		p = new double[] { 100, 0 };
 		angle = KineMath.bestAngleInDeg(p, 10, 90);
 		assertEqual("Best angle straight right", 180, angle, 0.1);
-		
 
-		p = new double[] {25,25};
-		angle = KineMath.bestAngleInDeg(p, 5,5);
-		assertEqual("Best angle up right", 225, angle, 0.1);		
+		p = new double[] { 25, 25 };
+		angle = KineMath.bestAngleInDeg(p, 5, 5);
+		assertEqual("Best angle up right", 225, angle, 0.1);
 
-		p = new double[] {-100,100};
-		angle = KineMath.bestAngleInDeg(p, 7,3);
+		p = new double[] { -100, 100 };
+		angle = KineMath.bestAngleInDeg(p, 7, 3);
 		assertEqual("Best angle up left", 315, angle, 0.1);
 
-		p = new double[] {-100,-100};
-		angle = KineMath.bestAngleInDeg(p, 10,1);
+		p = new double[] { -100, -100 };
+		angle = KineMath.bestAngleInDeg(p, 10, 1);
 		assertEqual("Best angle down left", 45, angle, 0.1);
+
+		p = new double[] { 100, -100 };
+		angle = KineMath.bestAngleInDeg(p, 10, 1);
+		assertEqual("Best angle down right", 135, angle, 0.1);
 		
 
-		p = new double[] {100,-100};
-		angle = KineMath.bestAngleInDeg(p, 10,1);
-		assertEqual("Best angle down right", 135, angle, 0.1);
+		double x = Math.sqrt(5*5 + 6*6);
+		p = new double[] { x, x };
+		angle = KineMath.bestAngleInDeg(p, 5, 6);
+		assertEqual("Best angle just fits", 225, angle, 0.1);
+
+		angle = KineMath.bestAngleInDeg(p, 5, 7);
+		assertEqual("Best angle just doesnt fit", 197.5, angle, 0.1);
+		
+
+		double swa = 15.5;
+		double ora = 555;
+
+		x = Math.sqrt(ora*ora + swa*swa);
+		p = new double[] { x, x };
+		angle = KineMath.bestAngleInDeg(p, swa, ora);
+		assertEqual("Best angle another just fit", 225, angle, 1);
 	}
+
 	public void testEtsiKulmat() {
-		double[] p = {0,10};
+		double[] p = { 0, 10 };
 		double[] ang = KineMath.etsikulmat(p, 5, 0);
 		assertEqual("etsiKulmat straight up", 90, ang[0], 0.1);
 		assertEqual("etsiKulmat straight up", 0, ang[1], 0.1);
-		
 
-		p = new double[] {0,-10};
+		p = new double[] { 0, -10 };
 		ang = KineMath.etsikulmat(p, 5, 0);
-		assertEqual("etsiKulmat straight down", 270, ang[0], 0.1);
+		assertEqual("etsiKulmat straight down", -90, ang[0], 0.1);
 		assertEqual("etsiKulmat straight down", 0, ang[1], 0.1);
-		p = new double[] {-10,0};
+		p = new double[] { -10, 0 };
 		ang = KineMath.etsikulmat(p, 5, 0);
-		assertEqual("etsiKulmat straight left", 180, ang[0], 0.1);
-		assertEqual("etsiKulmat straight left", 0, ang[1], 0.1);
-		p = new double[] {10,0};
+		assertEqual("etsiKulmat straight left", -180, ang[0], 0.1);
+		assertEqual("etsiKulmat straight left", 360, ang[1], 0.1);
+		p = new double[] { 10, 0 };
 		ang = KineMath.etsikulmat(p, 5, 0);
 		assertEqual("etsiKulmat straight right", 0, ang[0], 0.1);
 		assertEqual("etsiKulmat straight right", 0, ang[1], 0.1);
 
-		p= new double[] {5,5};
+		p = new double[] { 5, 5 };
 		ang = KineMath.etsikulmat(p, 5, 5);
-		assertEqual("etsiKulmat L up right", 0, ang[0], 0.1);
-		assertEqual("etsiKulmat L up right'", 90, ang[1], 0.1);
+		assertEqual("etsiKulmat L up right", 90, ang[0], 0.1);
+		assertEqual("etsiKulmat L up right'", -90, ang[1], 0.1);
 		
+		p = new double[] { 5, 5 };
+		ang = KineMath.etsikulmat(p, 5, 5, new double[] {-1,1}, new double[] {89,91});
+		assertEqual("etsiKulmat constrained L up right", 0, ang[0], 0.1);
+		assertEqual("etsiKulmat constrained L up right'", 90, ang[1], 0.1);
 
 		double armFromSwiwel = 4.2;
 		double armFromOrigo = 6;
 
-		double[] nearRange = {0,180};
-		double[] farRange = {-160,90};
-		
-		p = new double[] {0,4.2};
-		ang = KineMath.etsikulmat(p, armFromSwiwel, armFromOrigo, nearRange, farRange);
+		double[] nearRange = { 0, 180 };
+		double[] farRange = { -160, 90 };
+
+		p = new double[] { 0, 4.2 };
+		ang = KineMath.etsikulmat(p, armFromSwiwel, armFromOrigo, nearRange,
+				farRange);
 		assertEqual("Particular test case a", 135, ang[0], 1);
 		assertEqual("Particular test case a", -135, ang[1], 1);
-		
 
 		armFromSwiwel = 15.5;
-		armFromOrigo = Math.sqrt(17 * 17 + 4 * 4);;
+		armFromOrigo = Math.sqrt(17 * 17 + 4 * 4);
 
-		nearRange = new double[] {0,180};
-		farRange = new double[] {-150,160};
-		
-		p = new double[] {0,7};
-		ang = KineMath.etsikulmat(p, armFromSwiwel, armFromOrigo, nearRange, farRange);
-		assertEqual("Particular test case b", 27.7, ang[0], 1);
-		assertEqual("Particular test case b", 156.4, ang[1], 1);
+		nearRange = new double[] { -180, 180 };
+		farRange = new double[] { -180, 180 };
 
-		double x = Math.sqrt(armFromOrigo*armFromOrigo + armFromSwiwel*armFromSwiwel);
-		p = new double[] {x,x};
-		ang = KineMath.etsikulmat(p, armFromSwiwel, armFromOrigo, nearRange, farRange);
-		assertEqual("Particular test case d", 45, ang[0], 1);
-		assertEqual("Particular test case d", 0, ang[1], 1);
+		p = new double[] { 0, 7 };
+		ang = KineMath.etsikulmat(p, armFromSwiwel, armFromOrigo, nearRange,
+				farRange);
+		assertEqual("Particular test case b", 156, ang[0], 1);
+		assertEqual("Particular test case b", -155.6, ang[1], 1);
+
+		double x = Math.sqrt(armFromOrigo * armFromOrigo + armFromSwiwel
+				* armFromSwiwel);
+		p = new double[] { x, x };
+		ang = KineMath.etsikulmat(p, armFromSwiwel, armFromOrigo, nearRange,
+				farRange);
+		assertEqual("Particular test case d",  45, ang[0], 1);
+		assertEqual("Particular test case d'", 0, ang[1], 1);
 	}
+
 	public void testBinSearchStraightUp() {
-		double[] p = {0,10};
+		double[] p = { 0, 10 };
 		double dist = 5;
-		double[] rp = KineMath.binSearchPointWithDist(p, dist, 0);
+		double[] rp = KineMath.bestPointCW(p, dist, 0);
 		assertEqual("bintest straight up", 0, rp[0], 0.1);
 		assertEqual("bintest straight up", 5, rp[1], 0.1);
 	}
 
 	public void testBinSearchStraightDown() {
-		double[] p = {0,-10};
+		double[] p = { 0, -10 };
 		double dist = 5;
-		double[] rp = KineMath.binSearchPointWithDist(p, dist, 0);
+		double[] rp = KineMath.bestPointCW(p, dist, 0);
 		assertEqual("bintest straight down", 0, rp[0], 0.1);
 		assertEqual("bintest straight down", -5, rp[1], 0.1);
 	}
+
 	public void testBinSearchStraightRight() {
-		double[] p = {12, 0};
+		double[] p = { 12, 0 };
 		double dist = 5;
-		double[] rp = KineMath.binSearchPointWithDist(p, dist, 0);
-		assertEqual("bintest straight right", 12-5, rp[0], 0.1);
+		double[] rp = KineMath.bestPointCW(p, dist, 0);
+		assertEqual("bintest straight right", 12 - 5, rp[0], 0.1);
 		assertEqual("bintest straight right", 0, rp[1], 0.1);
 	}
+
 	public void testBinSearchStraightLeft() {
-		double[] p = {-12, 0};
+		double[] p = { -12, 0 };
 		double dist = 5;
-		double[] rp = KineMath.binSearchPointWithDist(p, dist, 0);
-		assertEqual("bintest straight left", -12+5, rp[0], 0.1);
+		double[] rp = KineMath.bestPointCW(p, dist, 0);
+		assertEqual("bintest straight left", -12 + 5, rp[0], 0.1);
 		assertEqual("bintest straight left", 0, rp[1], 0.1);
 	}
+
 	public static void main(String[] args) {
 		KineMathTest t = new KineMathTest();
 		t.run();
 	}
+
 	public boolean equal(double d1, double d2, double tolerance) {
-		return (Math.abs(d1-d2) <= tolerance);
+		return (Math.abs(d1 - d2) <= tolerance);
 	}
-	public void assertEqual(String testName, double d1, double d2, double tolerance) {
-		if (!equal(d1,d2,tolerance))
+
+	public void assertEqual(String testName, double d1, double d2,
+			double tolerance) {
+		if (!equal(d1, d2, tolerance))
 			fail(testName, d1, d2);
 	}
+
 	public void fail(String testName, double expected, double got) {
-		System.out.println("Failed test: " + testName + "; expected " + expected + ", got " + got);
+		System.out.println("Failed test: " + testName + "; expected "
+				+ expected + ", got " + got);
 	}
+
 	public void testAngleBetween00xx() {
-		for (int i = 1; i<100; i++) {
-			double[] p1 = {0,0};
-			double[] p2 = {i,i};
-			double result = Math.toDegrees(KineMath.angleBetween(p1,p2));
+		for (int i = 1; i < 100; i++) {
+			double[] p1 = { 0, 0 };
+			double[] p2 = { i, i };
+			double result = Math.toDegrees(KineMath.angleBetween(p1, p2));
 			assertEqual("Angle between (0,0) and (x,x)", 45, result, 0.1);
 		}
 	}
+
 	public void testAngleBetweenxx00() {
-		for (int i = 1; i<100; i++) {
-			double[] p1 = {i,i};
-			double[] p2 = {0,0};
-			double result = Math.toDegrees(KineMath.angleBetween(p1,p2));
+		for (int i = 1; i < 100; i++) {
+			double[] p1 = { i, i };
+			double[] p2 = { 0, 0 };
+			double result = Math.toDegrees(KineMath.angleBetween(p1, p2));
 			assertEqual("Angle between (0,0) and (x,x)", -135, result, 0.1);
 		}
 	}
 
 	public void testAngleBetween0x00() {
-		for (int i = 1; i<100; i++) {
-			double[] p1 = {0,i};
-			double[] p2 = {0,0};
-			double result = Math.toDegrees(KineMath.angleBetween(p1,p2));
+		for (int i = 1; i < 100; i++) {
+			double[] p1 = { 0, i };
+			double[] p2 = { 0, 0 };
+			double result = Math.toDegrees(KineMath.angleBetween(p1, p2));
 			assertEqual("Angle between (0,0) and (x,x)", -90, result, 0.1);
 		}
 	}
-	
+
 	public void armStraightToRight() {
-		
+
 	}
 }
