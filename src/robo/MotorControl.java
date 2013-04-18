c84433e942fa52e0318f2912a387bb512e1d11ae
Mikko Tamminen <pahanaama@gmail.com>
2013-04-18 19:45:51 +0300

    refactor

--- src/robo/MotorControl.java
+++ src/robo/MotorControl.java
@@ -8,20 +8,20 @@ import lejos.nxt.Sound;
 
 public class MotorControl {
 
-	NXTRegulatedMotor mnear;
-	NXTRegulatedMotor mfar;
-	int nearspeed = 300;
-	int farspeed = 300;
-	int gearRatio = 3;
+	private NXTRegulatedMotor mnear;
+	private NXTRegulatedMotor mfar;
+	private int nearspeed = 100;
+	private int farspeed = 100;
+	private int gearRatio = 3;
 	private boolean penUp;
 
-	double armFromSwiwel = 11.3;
-	double armFromOrigo = 13.4;
+	private double armFromSwiwel = 11.3;
+	private double armFromOrigo = 13.4;
 	
-	double[] nearRange = {-180,180};
-	double[] farRange = {-160,150};
+	private double[] nearRange = {-180,180};
+	private double[] farRange = {-160,150};
 	
-	double[] xy = new double[2];
+	private double[] xy = new double[2];
 
 	public MotorControl(MotorPort near, MotorPort far) {
 		mnear = new NXTRegulatedMotor(near);
@@ -49,7 +49,7 @@ public class MotorControl {
 		return xy;
 	}
 
-	public void rotateTo(double near, double far, boolean immediateReturn) {
+	private void rotateTo(double near, double far, boolean immediateReturn) {
 		mnear.rotateTo(-(int) (near * gearRatio), true);
 		mfar.rotateTo(-(int) (far * gearRatio), immediateReturn);
 	}
@@ -70,5 +70,66 @@ public class MotorControl {
 
 	public boolean isMoving() {
 		return mnear.isMoving() || mfar.isMoving();
+	}
+	
+	public void drawLinemap(float[][] linemap, float x, float y) {
+		for (float[] line : linemap) {
+			float x1 = x + line[0];
+			float y1 = y + line[1];
+			float x2 = x + line[2];
+			float y2 = y + line[3];
+			drawLine(x1, y1, x2, y2, 0.5f);
+		}
+	}
+	
+	public void lopeta() {
+		liftPen();
+		rotateTo(0, 0, false);
+		lowerPen();
+		System.exit(0);
+	}
+	
+	private static double dist(double x1, double y1, double x2, double y2) {
+		return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+	}
+	/**
+	 * 
+	 * @param m 
+	 * @param x1 
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @param maxSegment how long the 
+	 */
+	public void drawLine(float x1, float y1, float x2, float y2, float maxSegment) {
+		if (dist(getXy()[0], getXy()[1], x1, y1)<0.2) //jos lähtöpiste on kovin lähellä kynän nykyistä sijaintia
+			;
+		else
+			liftPen();
+
+		moveTo(x1, y1, false);
+		lowerPen();
+		
+		float linelenght = (float) dist(x1,y1,x2,y2);
+		int pieces = Math.round((float)Math.ceil(linelenght/maxSegment));
+		if (pieces < 1)
+			pieces = 1;
+		
+		float deltax = x2-x1;
+		float deltay = y2-y1;
+		
+		float piecex = deltax / pieces;
+		float piecey = deltay / pieces;
+		
+		for(int i=0; i<=pieces; i++) {
+			moveTo(x1+piecex*i,y1+piecey*i,false);
+		}
+		
+	}
+	public void drawRect(float x1, float y1, float x2, float y2, float step) {
+		drawLine(x1,y1,x1,y2, step);
+		drawLine(x1,y2,x2,y2, step);
+		drawLine(x2,y2,x2,y1, step);
+		drawLine(x2,y1,x1,y1, step);
 	}
 }
--- src/robo/RoboHand.java
+++ src/robo/RoboHand.java
@@ -41,64 +41,13 @@ public class RoboHand {
 		panicbutton.start();
 
 		MotorControl m = new MotorControl();
-		for(int i=0;i<5;i++)
-		hello(-5,12,m);
 		
-		lopeta(m);
+		m.drawLinemap(hello, -5,12);
+		
+		m.lopeta();
 	}
-	
-	public static void hello(float x, float y, MotorControl m) {
-		for (float[] line : hello) {
-			float x1 = x + line[0];
-			float y1 = y + line[1];
-			float x2 = x + line[2];
-			float y2 = y + line[3];
-			drawLine(m, x1, y1, x2, y2, 0.5f);
-		}
-	}
-	
 	public static void sisakkLaatikotDemo(MotorControl m) {
 		for(int i = 0; i<5; i++)
-	 		drawRect(m, 0+i, 10+i, 15-i, 20-i, 0.5f);	
-	}
-	
-	public static void lopeta(MotorControl m) {
-		m.liftPen();
-		m.rotateTo(0, 0, false);
-		m.lowerPen();
-		System.exit(0);
-	}
-	
-	private static double dist(double x1, double y1, double x2, double y2) {
-		return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-	}
-	public static void drawLine(MotorControl m, float x1, float y1, float x2, float y2, float step) {
-		if (dist(m.getXy()[0], m.getXy()[1], x1, y1)<0.2) //jos lähtöpiste on kovin lähellä kynän nykyistä sijaintia
-			;
-		else
-			m.liftPen();
-
-		m.moveTo(x1, y1, false);
-		m.lowerPen();
-		
-		float linelenght = (float) dist(x1,y1,x2,y2);
-		int pieces = Math.round((float)Math.ceil(linelenght/step));
-		
-		float deltax = x2-x1;
-		float deltay = y2-y1;
-		
-		float piecex = deltax / pieces;
-		float piecey = deltay / pieces;
-		
-		for(int i=0; i<=pieces; i++) {
-			m.moveTo(x1+piecex*i,y1+piecey*i,false);
-		}
-		
-	}
-	public static void drawRect(MotorControl m, float x1, float y1, float x2, float y2, float step) {
-		drawLine(m,x1,y1,x1,y2, step);
-		drawLine(m,x1,y2,x2,y2, step);
-		drawLine(m,x2,y2,x2,y1, step);
-		drawLine(m,x2,y1,x1,y1, step);
+	 		m.drawRect(0+i, 10+i, 15-i, 20-i, 0.5f);	
 	}
 }
